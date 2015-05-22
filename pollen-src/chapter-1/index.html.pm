3c161c6dbfdeebc9980a5a07513220e52f40e173
Thomas Seng Hin Mak <mak@makzan.net>
2015-05-22 08:39:23 +0000

    Updated chapter 1 to add flex direction

--- pollen-src/chapter-1/index.html.pm
+++ pollen-src/chapter-1/index.html.pm
@@ -3,8 +3,9 @@
 ◊headline{Chapter 1—Introducing Flexbox}
 
 ◊(toclist
+  ◊tocitem["./flex-direction.html"]{Flex direction}
   ◊tocitem["./center-align.html"]{Center alignment}
-  ◊tocitem["./inputs.html"]{Inputs}
   ◊tocitem["./container-and-items.html"]{Container and items}
   ◊tocitem["./flex-grow-and-shrink.html"]{Flex grow and shrink}
+  ◊tocitem["./inputs.html"]{Inputs}
 )

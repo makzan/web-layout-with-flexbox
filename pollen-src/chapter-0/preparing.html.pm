2d255ca4c78b442cec946d3463fec48f05a5c268
Thomas Seng Hin Mak <mak@makzan.net>
2015-05-23 04:19:58 +0000

    Fixed typo

--- pollen-src/chapter-0/preparing.html.pm
+++ pollen-src/chapter-0/preparing.html.pm
@@ -2,7 +2,7 @@
 
 ◊headline{Preparing for the workshop}
 
-You’ll need to bring your laptop with a modern browser and your favorite code editor installed. During the workshop, there are several code demos. I put both the code into the page and on Codepen is live demo. You can find thoes live demo in the following Codpen collection. Please fork them and play around the code yourself. You won’t learn it until you hack it.
+You’ll need to bring your laptop with a modern browser and your favorite code editor installed. During the workshop, there are several code demos. I put both the code into the page and on Codepen is live demo. You can find those live demos in the following Codpen collection. Please fork them and play around the code yourself. You won’t learn it until you hack it.
 
 ◊link["http://codepen.io/collection/XdbWMW/"]{Code examples on CodePen}
 
@@ -12,5 +12,5 @@ You’ll need to bring your laptop with a modern browser and your favorite code 
 
 ◊section{Preprocessor}
 
-The logic in CSS preprocessor provides us the DRY—Don’t repeat yourself— way to write the CSS code. We will use some SCSS code when defining the grid layout. It’s loop and list feature helps reducing the lines of code a lot. If you are using other preprocessor, you may find their equivalents in ◊link["http://pre.com"]{CSSPre.com}. If you aren’t using any preprocessor, no worry, we also provide the full CSS code. 
+The logic in CSS preprocessor provides us the DRY—Don’t repeat yourself— way to write the CSS code. We will use some SCSS code when defining the grid layout. It’s loop and list feature helps reducing the lines of code a lot. If you are using other preprocessor, you may find their equivalents in ◊link["http://csspre.com"]{CSSPre.com}. If you aren’t using any preprocessor, no worry, we also provide the full CSS code. 
 

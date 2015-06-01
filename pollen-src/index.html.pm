7a8e2142cd811a861a0957584c684e47c823cd4a
Thomas Seng Hin Mak <mak@makzan.net>
2015-06-01 08:16:25 +0000

    Added version to footer

--- pollen-src/index.html.pm
+++ pollen-src/index.html.pm
@@ -8,7 +8,9 @@ This is the material I originally created for workshop “◊strong{Web Layout w
 ◊(toclist
   ◊tocitem["/chapter-0/"]{Chapter 0—Preparation}
   ◊tocitem["/chapter-1/"]{Chapter 1—Introducing Flexbox}
-  ◊tocitem["/chapter-2"]{Chapter 2—Row–Column grid layout}
-  ◊tocitem["/chapter-3"]{Chapter 3—Grid layout, powered by FlexBox}
+  ◊tocitem["/chapter-2/"]{Chapter 2—Row–Column grid layout}
+  ◊tocitem["/chapter-3/"]{Chapter 3—Grid layout, powered by FlexBox}
+  ◊tocitem["/summary.html"]{Summary}
+  ◊tocitem["/appendex/"]{Appendix}
 )
 

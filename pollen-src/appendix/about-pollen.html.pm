9aa8983ad240270bfd776d74f30b420cda369c1f
Thomas Seng Hin Mak <mak@makzan.net>
2015-06-01 08:25:30 +0000

    Updated About Pollen page

--- pollen-src/appendix/about-pollen.html.pm
+++ pollen-src/appendix/about-pollen.html.pm
@@ -2,4 +2,4 @@
 
 ◊headline{About Pollen}
 
-If you check the source code of this website, you may wonder why the HTML contains non-standard HTML tag. It’s because I compose this website by using the ◊link["http://pollenpub.com"]{Pollen}—A racket package that encourages writing books like a program.
+If you check the source code of this website, you may wonder why the HTML contains non-standard HTML tag. It’s because I compose this website by using the ◊link["http://pollenpub.com"]{Pollen}—A racket package that encourages writing books like a program. I’m still learning how to use Pollen and output valid HTML code. 
